a883faf326d985fc3023daf621199fe5b652fb35
seyeoyadiran <seyeoyadiran@gmail.com>
2024-12-19 03:39:52 -0500

    Right Side Bar Update

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,9 +1,85 @@
 import "./rightbar.css";
+import Gym from "./img/esporta.jpg"
+import Ava from "./img/avaimg.png"
 
 export default function Rightbar() {
   return (
     <div className="rightbar">
-        Right bar
+        <div className="rightbarWrapper">
+          <div className="birthdayContainer">
+            <img src="" alt="" className="birthdayImg" />
+            <span className="birthdayText">
+              <b>Seye O</b> and <b> 6 other friends </b> started a workout today
+            </span>
+          </div>
+          <img src={Gym} alt="" className="rightBarAd"  />
+          <h4 className="rightbarTitle"> Online Friends </h4>
+          <ul className="rightbarFriendList">
+            <li className="rightBasrFriend"> 
+              <div className="rightbarProfileImgContainer">
+                <img src={Ava} alt="" className="rightbarProfileImg" />
+                <span className="rightbarOnline"> </span>
+              </div>
+              <span className="rightbarUsername"> John Carter</span>
+            </li>
+            <li className="rightBasrFriend"> 
+              <div className="rightbarProfileImgContainer">
+                <img src={Ava} alt="" className="rightbarProfileImg" />
+                <span className="rightbarOnline"> </span>
+              </div>
+              <span className="rightbarUsername"> John Carter</span>
+            </li>
+            <li className="rightBasrFriend"> 
+              <div className="rightbarProfileImgContainer">
+                <img src={Ava} alt="" className="rightbarProfileImg" />
+                <span className="rightbarOnline"> </span>
+              </div>
+              <span className="rightbarUsername"> John Carter</span>
+            </li>
+            <li className="rightBasrFriend"> 
+              <div className="rightbarProfileImgContainer">
+                <img src={Ava} alt="" className="rightbarProfileImg" />
+                <span className="rightbarOnline"> </span>
+              </div>
+              <span className="rightbarUsername"> John Carter</span>
+            </li>
+            <li className="rightBasrFriend"> 
+              <div className="rightbarProfileImgContainer">
+                <img src={Ava} alt="" className="rightbarProfileImg" />
+                <span className="rightbarOnline"> </span>
+              </div>
+              <span className="rightbarUsername"> John Carter</span>
+            </li>
+            <li className="rightBasrFriend"> 
+              <div className="rightbarProfileImgContainer">
+                <img src={Ava} alt="" className="rightbarProfileImg" />
+                <span className="rightbarOnline"> </span>
+              </div>
+              <span className="rightbarUsername"> John Carter</span>
+            </li>
+            <li className="rightBasrFriend"> 
+              <div className="rightbarProfileImgContainer">
+                <img src={Ava} alt="" className="rightbarProfileImg" />
+                <span className="rightbarOnline"> </span>
+              </div>
+              <span className="rightbarUsername"> John Carter</span>
+            </li>
+            <li className="rightBasrFriend"> 
+              <div className="rightbarProfileImgContainer">
+                <img src={Ava} alt="" className="rightbarProfileImg" />
+                <span className="rightbarOnline"> </span>
+              </div>
+              <span className="rightbarUsername"> John Carter</span>
+            </li>
+            <li className="rightBasrFriend"> 
+              <div className="rightbarProfileImgContainer">
+                <img src={Ava} alt="" className="rightbarProfileImg" />
+                <span className="rightbarOnline"> </span>
+              </div>
+              <span className="rightbarUsername"> John Carter</span>
+            </li>
+          </ul>
+        </div>
     </div>
   )
 }
